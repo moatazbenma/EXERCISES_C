66358713386dc6865063331c1e96a02444d267de
moataz <moatazbanmansour@gmail.com>
2024-12-30 13:12:48 +0100

    C

--- C/LOOP/COMPARE NUMBERS.c
+++ C/LOOP/COMPARE NUMBERS.c
@@ -1,48 +1,29 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
-// Function prototype
-int mama(int a, int b);
+int max_of_four(int a, int b, int c, int d);
 
 int main() {
-    int largest;
-    int smallest;
-    int a, b, c;
-
-    // Input three numbers
-    printf("Enter the 1. number: ");
-    scanf("%d", &a);
-    printf("Enter the 2. number: ");
-    scanf("%d", &b);
-    printf("Enter the 3. number: ");
-    scanf("%d", &c);
-    
-    // Initialize largest and smallest with the first number
-    largest = smallest = a;
-
-    // Compare to find the largest
-    if (b > largest) {
-        largest = b;
-    }
-    if (c > largest) {
-        largest = c;
-    }
-
-    // Compare to find the smallest
-    if (b < smallest) {
-        smallest = b;
-    }
-    if (c < smallest) {
-        smallest = c;
-    }
-
-    // Call the function to print the result
-    mama(largest, smallest);
-
-    return 0; // Return from main
+    int a, b, c, d;
+    printf(" ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("\n%d", ans);
+    return 0;
 }
 
-// Function to print the largest and smallest values
-int mama(int a, int b) {
-    printf("The largest was %i and the smallest was %i\n", a, b);
-    return 0;
+int max_of_four(int a, int b, int c, int d){
+    int greatest = a;
+    for(int i = 0;i < 4; i++){
+        if(b > greatest){
+            greatest = b;
+        }
+        if(c > greatest){
+            greatest = c;
+        }
+        if(d > greatest){
+            greatest = d;
+        }
+    }
+    return greatest;
 }
